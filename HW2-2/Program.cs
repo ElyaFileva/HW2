7575934f8d38f30feffff69f0d7879ec749123be
Elya Fileva <filevaelya@mail.ru>
2023-02-16 16:02:43 +0300

    добавлена задача 2-2

---
+++ HW2-2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num / 10 == 0 || num / 100 == 0)
+{
+  Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+  while (num != 0)
+  {
+    if (num / 1000 == 0)
+    {
+      Console.WriteLine(Math.Abs(num % 10));
+      break;
+    }
+    else
+    {
+      num = num / 10;
+    }
+  }
+}
