18633a54abf128ac6227120df632968729e81968
Elya Fileva <filevaelya@mail.ru>
2023-02-16 16:05:21 +0300

    добавлена задача 2-3

---
+++ HW2-3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+int num = new Random().Next(1, 8);
+bool day = true;
+Console.Write($"Число: {num}, выходной день: ");
+if (num >= 6)
+  Console.WriteLine(day);
+else
+  Console.WriteLine(!day);
